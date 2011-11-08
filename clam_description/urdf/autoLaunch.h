75e2183b448f97120fa448d7878bb5f05b26c15d
Dave Coleman <davetcoleman@gmail.com>
2011-11-08 14:03:28 -0700

    Finished URDF for block robot arm

--- clam_description/urdf/autoLaunch.h
+++ clam_description/urdf/autoLaunch.h
@@ -1,2 +1,3 @@
 ~/ros/xacro/xacro.py clam_urdf.xarco > clam_urdf_generated.xml;
 rosrun urdf_parser check_urdf clam_urdf_generated.xml;
+roslaunch clam_description clam_description.launch;
--- clam_description/urdf/launchRviz.h
+++
@@ -1,1 +0,0 @@
-roslaunch urdf_tutorial display.launch model:=clam_urdf.xml;
