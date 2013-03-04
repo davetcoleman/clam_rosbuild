8bb7d84c914d32896e1623cef81186bc31d9660c
Dave Coleman <davetcoleman@gmail.com>
2013-03-04 15:41:40 -0700

    Using Fuerte to convert URDF to Collada

---
+++ clam_description/openrave/batch_run.sh
@@ -0,0 +1,8 @@
+python /usr/lib/python2.7/dist-packages/openravepy/_openravepy_0_8/ikfast.py --robot=clam.dae --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61_0.cpp --freeindex=0
+python /usr/lib/python2.7/dist-packages/openravepy/_openravepy_0_8/ikfast.py --robot=clam.dae --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61_1.cpp --freeindex=1
+python /usr/lib/python2.7/dist-packages/openravepy/_openravepy_0_8/ikfast.py --robot=clam.dae --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61_2.cpp --freeindex=2
+python /usr/lib/python2.7/dist-packages/openravepy/_openravepy_0_8/ikfast.py --robot=clam.dae --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61_3.cpp --freeindex=3
+python /usr/lib/python2.7/dist-packages/openravepy/_openravepy_0_8/ikfast.py --robot=clam.dae --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61_4.cpp --freeindex=4
+python /usr/lib/python2.7/dist-packages/openravepy/_openravepy_0_8/ikfast.py --robot=clam.dae --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61_5.cpp --freeindex=5
+python /usr/lib/python2.7/dist-packages/openravepy/_openravepy_0_8/ikfast.py --robot=clam.dae --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61_6.cpp --freeindex=6
+python /usr/lib/python2.7/dist-packages/openravepy/_openravepy_0_8/ikfast.py --robot=clam.dae --iktype=transform6d --baselink=0 --eelink=7 --savefile=output_ikfast61_7.cpp --freeindex=7
